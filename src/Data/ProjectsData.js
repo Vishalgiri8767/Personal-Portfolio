2b6694ea94fd49b766c88ba1181bf3803f2e7d49
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-03-22 18:30:48 +0530

    project git link added

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -2,7 +2,7 @@ const DUMMY_PROJECTS=[
     {
         projectTitle:'PortFolio Template',
         description: 'This project is a React app to generate a creative professional website based on Data provided. The website uses no backend and is free to fork and use.',
-        sourceLink:'https://github.com/killer-abhi/Portfolio.git',
+        sourceLink:'https://github.com/Vishalgiri8767/Personal-Portfolio',
         lastUpdated:'Jan 20 2023'
     },
     {
